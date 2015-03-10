2bd674ad5df1b634199d84ec33f93e3633c78881
Haroldo Ramirez <haroldoramirez@gmail.com>
2015-03-10 13:40:31 -0300

    melhorado active do menu

--- public/javascripts/active.js
+++ public/javascripts/active.js
@@ -1,6 +1,23 @@
-  $(function () {
+//Créditos Bacon
+$(function () {
         $('.navbar-nav li').click(function () {
             $(this).siblings().removeClass('active');
             $(this).addClass('active');
         });
-    });
+});
+
+var dropdownArray = document.getElementsByClassName("dropdown");
+window.addEventListener("click",function(){
+    setTimeout(function(){
+        for(var c,i=dropdownArray.length; i--;){
+            if((c=dropdownArray[i].getAttribute("class")).indexOf("active")>=0&&c.indexOf("open")==-1){
+                dropdownArray[i].setAttribute("class",c.replace(" active",""));
+            }
+        }
+    },0);
+});
+
+//em jquery
+/*$(document).click(function(){
+    $(".dropdown.active").not(".open").removeClass("active");
+});*/
